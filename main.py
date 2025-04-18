df13fb76d41cdfd457e9289c333fa5a3cdcf2884
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-18 06:49:46 +0000

    gghgcg

--- main.py
+++ main.py
@@ -119,12 +119,5 @@ async def get_product(product_id: int):
         return ProductOut.model_validate(product)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "main:app",  # имя файла:имя переменной с FastAPI
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000))
-    )
 
 
