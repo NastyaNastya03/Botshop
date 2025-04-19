498fcb64d9a38357206e7e8c19de4fc9fdfd90b8
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-19 12:23:01 +0500

    Update main.py
    оо

--- main.py
+++ main.py
@@ -128,5 +128,8 @@ async def get_product(product_id: int):
         return ProductOut.model_validate(product)
 
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 
