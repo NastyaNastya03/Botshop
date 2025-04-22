32b515c2eaf9b381931dfec661b2e799c692ced0
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 18:28:10 +0500

    Create user.py
    с

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import BigInteger, String
+from sqlalchemy.orm import Mapped, mapped_column
+from . import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[BigInteger] = mapped_column()
+    role: Mapped[str] = mapped_column(String(10))
+
+class Admin(User):
+    __tablename__ = "admins"
+
+    id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
