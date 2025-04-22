60cd4a02b64713dadd3c7c1f44baa37f0459196a
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 19:22:10 +0500

    Update __init__.py
    ч

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,17 @@
+from .base import Base, engine, async_session, get_async_session, init_db
 from .user import User, Admin
 from .product import Product
 from .order import Order, OrderProducts
 
+__all__ = [
+    'Base',
+    'engine',
+    'async_session',
+    'get_async_session',
+    'init_db',
+    'User',
+    'Admin',
+    'Product',
+    'Order',
+    'OrderProducts'
+]
