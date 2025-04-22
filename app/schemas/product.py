bf5af1594a4b9e9e5277f1787335891c88f32bb2
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 18:49:20 +0500

    Update product.py
    все про товар

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,1 +1,41 @@
-ddd
+from pydantic import BaseModel
+from typing import Optional, List
+from datetime import date
+from decimal import Decimal
+
+class CreateProduct(BaseModel):
+    tg_id: int
+    title: str
+    category: str
+    price: Decimal
+    size: int
+    color: str
+    quantity: int
+    image_url: Optional[str]
+    
+class CompleteProduct(BaseModel):
+    id: int
+
+class UpdateProduct(BaseModel):
+    id: int
+    title: str
+    category: str
+    price: Decimal
+    size: int
+    color: str
+    quantity: int
+    image_url: Optional[str] = None
+    tg_id: int
+
+class ProductOut(BaseModel):
+    id: int
+    title: str
+    category: str
+    price: Decimal
+    size: int
+    color: str
+    quantity: int
+    image_url: Optional[str] = None
+    class Config:
+        from_attributes = True
+
