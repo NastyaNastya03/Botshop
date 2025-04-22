658c3dc7103b7ab5a2c1d9bab32139b0c63ba285
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 19:34:02 +0500

    Update admin.py
    d

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import Admin, User
+from .dependencies import get_db
 
-router = APIRouter()
+router = APIRouter(prefix="/admin", tags=["admin"])
 
 @router.get("/check-admin/{tg_id}")
-async def check_admin(tg_id: int):
-    return {"isAdmin": True}
+async def check_admin(
+    tg_id: int,
+    db: AsyncSession = Depends(get_db)
+):
+    user = await db.get(User, tg_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    is_admin = await db.get(Admin, user.id) is not None
+    return {"isAdmin": is_admin}
