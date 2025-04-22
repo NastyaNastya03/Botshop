ddc31fca9a676b5e09b6fcfc715362f7aaec7de0
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 20:51:22 +0500

    Update order.py
    c

--- app/routers/order.py
+++ app/routers/order.py
@@ -1,23 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.order import CreateOrder, OrderOut, CompleteOrder
-from app.services.order_service import (
-    create_order,
-    get_user_orders,
-    complete_order
-)
+from app.schemas.order import OrderCreate, OrderOut
+from app.services.order_service import OrderService
 from app.models.db import get_async_session
 
-router = APIRouter(prefix="/api/orders", tags=["Orders"])
+router = APIRouter(prefix="/orders", tags=["orders"])
 
 @router.get("/{tg_id}", response_model=list[OrderOut])
 async def get_orders_by_user(tg_id: int, db: AsyncSession = Depends(get_async_session)):
     return await get_user_orders(db, tg_id)
 
-@router.post("/", status_code=201)
-async def create_new_order(order: CreateOrder, db: AsyncSession = Depends(get_async_session)):
-    await create_order(db, **order.model_dump())
-    return {"status": "created"}
+@router.post("/", response_model=OrderOut)
+async def create_order(
+    order_data: OrderCreate,
+    db: AsyncSession = Depends(get_async_session)
+):
+    try:
+        return await OrderService(db).create_order(order_data)
+    except ValueError as e:
+        raise HTTPException(400, str(e))
 
 @router.patch("/complete")
 async def mark_order_completed(order: CompleteOrder, db: AsyncSession = Depends(get_async_session)):
