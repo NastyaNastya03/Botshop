343683b994041e117333e83d4f23d6ea751325ad
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 20:39:08 +0500

    Create product.py
    м

---
+++ app/routers/product.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.product import ProductOut, CreateProduct, UpdateProduct, CompleteProduct
+from app.services.product_service import (
+    get_all_products,
+    get_product_by_id,
+    create_product,
+    update_product_data,
+    complete_product
+)
+from app.models.db import get_async_session
+
+router = APIRouter(prefix="/api/products", tags=["Products"])
+
+@router.get("/", response_model=list[ProductOut])
+async def list_products(db: AsyncSession = Depends(get_async_session)):
+    return await get_all_products(db)
+
+@router.get("/{product_id}", response_model=ProductOut)
+async def get_product_details(product_id: int, db: AsyncSession = Depends(get_async_session)):
+    product = await get_product_by_id(db, product_id)
+    if not product:
+        raise HTTPException(404, "Product not found")
+    return product
+
+@router.post("/", status_code=201)
+async def add_new_product(product: CreateProduct, db: AsyncSession = Depends(get_async_session)):
+    await create_product(db, **product.model_dump())
+    return {"status": "created"}
+
+@router.patch("/complete")
+async def mark_product_completed(product: CompleteProduct, db: AsyncSession = Depends(get_async_session)):
+    await complete_product(db, product.id)
+    return {"status": "completed"}
+
+@router.patch("/update")
+async def update_product_info(product: UpdateProduct, db: AsyncSession = Depends(get_async_session)):
+    await update_product_data(db, product)
+    return {"status": "updated"}
