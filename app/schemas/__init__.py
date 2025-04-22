832f56c52d19a53b4b12afa4578e165f61ead54c
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 19:42:34 +0500

    Update __init__.py
    v

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,12 @@
-ss
+from .order import *
+from .product import *
+
+__all__ = [
+    'CreateOrder',
+    'CompleteOrder',
+    'OrderOut',
+    'CreateProduct',
+    'CompleteProduct',
+    'UpdateProduct',
+    'ProductOut'
+]
