ee7ec3df18baac296d95d4f760842bf3f4d0e8c3
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 19:51:23 +0500

    Update user_service.py
    d

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,27 +1,17 @@
-from app.models import User, Admin
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
 
-from sqlalchemy import select, update
-from models import async_session, User, Order, Product
-from pydantic import BaseModel, ConfigDict
-from datetime import date
-from decimal import Decimal
-from typing import List, Optional
-from schemas import ProductOut, OrderOut, UpdateProduct
-from fastapi import HTTPException
-
-class BaseModelWithConfig(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-   
-async def add_user(tg_id) -> User:
-    async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
-        if user:
-            return user
-        
-        new_user = User(tg_id=tg_id)
-        session.add(new_user)
-        await session.commit()
-        await session.refresh(new_user)
-        return new_user
+async def add_user(session: AsyncSession, tg_id: int) -> User:
+    """Добавляет нового пользователя или возвращает существующего"""
+    user = await session.scalar(select(User).where(User.tg_id == tg_id))
+    if user:
+        return user
+    
+    new_user = User(tg_id=tg_id)
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
 
 
