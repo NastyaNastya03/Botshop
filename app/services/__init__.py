54f06ee6bc40550e22f039b0731912825ad31adf
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 19:53:20 +0500

    Update __init__.py
    d

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,19 @@
-s
+from .user_service import add_user
+from .product_service import (
+    get_all_products,
+    create_product,
+    update_product_data,
+    update_product_quantity
+)
+from .order_service import get_orders, create_order, update_order
+
+__all__ = [
+    'add_user',
+    'get_all_products',
+    'create_product',
+    'update_product_data',
+    'update_product_quantity',
+    'get_orders',
+    'create_order',
+    'update_order'
+]
