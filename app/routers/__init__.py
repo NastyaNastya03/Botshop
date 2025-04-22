f4a6b2998607603e056e9ebbcced370a78364f74
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-22 19:31:42 +0500

    Update __init__.py
    с

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,1 +1,11 @@
-cc
+from fastapi import APIRouter
+from .admin import router as admin_router
+from .order import router as order_router
+from .upload import router as upload_router
+
+router = APIRouter()
+router.include_router(admin_router)
+router.include_router(order_router)
+router.include_router(upload_router)
+
+__all__ = ["router"]
