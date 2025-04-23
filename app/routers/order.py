19b7cef113889225bdd4228ff8562035a7c1ad39
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-23 19:38:20 +0500

    Update order.py

--- app/routers/order.py
+++ app/routers/order.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.order import OrderCreate, OrderOut
+from app.schemas.order import CreateOrder, OrderOut, CompleteOrder
 from app.services.order_service import OrderService
-from app.models.db import get_async_session
+import services.order_service as orderser
+from app.models.base import get_async_session
 
 router = APIRouter(prefix="/orders", tags=["orders"])
 
 @router.get("/{tg_id}", response_model=list[OrderOut])
 async def get_orders_by_user(tg_id: int, db: AsyncSession = Depends(get_async_session)):
-    return await get_user_orders(db, tg_id)
+    return await orderser.get_orders(db, tg_id)
 
 @router.post("/", response_model=OrderOut)
 async def create_order(
-    order_data: OrderCreate,
+    order_data: CreateOrder,
     db: AsyncSession = Depends(get_async_session)
 ):
     try:
