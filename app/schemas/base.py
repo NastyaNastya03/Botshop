3875b93af90c77c2875011725d0ca593b6e5c0dc
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-24 12:49:07 +0500

    Delete app/schemas directory
    hn

--- app/schemas/base.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from pydantic import ConfigDict
-
-class BaseModelWithConfig(BaseModel):
-    """Базовая модель с включённой конфигурацией для работы с ORM"""
-    model_config = ConfigDict(from_attributes=True)
