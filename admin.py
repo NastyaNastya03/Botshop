17bbf644e1bfe6a7472f472ef3ef65631a9012cc
NastyaNastya03 <notylun.sky@gmail.com>
2025-04-24 12:50:10 +0500

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from models import get_async_session, User
+from requests import rq
+
+router = APIRouter(prefix="/admin")
+
+@router.get("/check-admin/{tg_id}")
+async def check_admin(
+    tg_id: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    user = await rq.add_user(tg_id)
+    if not user or user.role != "admin":
+        raise HTTPException(status_code=403, detail="Not an admin")
+    return {"isAdmin": True}
